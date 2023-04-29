80f6cb292c736f28cb5f482dc69db6e7d80391ad
Vishv Deshwal <vishavdeshwal@gmail.com>
2023-04-29 10:24:25 +0530

    addresses file is added

---
+++ address.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a =10 ;
+    float b = 2.1;
+    cout << "the adress of integer a is " << &a << endl;
+    cout << "the address of integer b is " << &b << endl;
+    return 0;
+}
